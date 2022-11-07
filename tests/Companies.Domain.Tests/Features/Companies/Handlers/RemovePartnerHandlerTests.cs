75cb36cf825a15e77ecabcf010cc305dea417f70
Tiago Santos <tfsantosbr@gmail.com>
2022-11-07 18:26:39 -0300

    remove partner tests

--- tests/Companies.Domain.Tests/Features/Companies/Handlers/RemovePartnerHandlerTests.cs
+++ tests/Companies.Domain.Tests/Features/Companies/Handlers/RemovePartnerHandlerTests.cs
@@ -21,32 +21,79 @@ public class RemovePartnerHandlerTests
     }
 
     [Fact]
-    public async Task ShouldReturnErrorResponseWhenRemoveCompanyPartnerInCompanyThatDoesntExist()
+    public async Task ShouldReturnErrorResponseWhenRemovePartnerInCompanyThatDoesntExist()
     {
         // arrange
 
+        var command = new RemovePartner();
+
+        var handler = new RemovePartnerHandler(
+            companyRepository: _companyRepository,
+            unitOfWork: _unitOfWork
+        );
+
         // act
 
+        var response = await handler.Handle(command, new CancellationToken());
+
         // assert
+
+        Assert.Contains(response.Notifications, notification => notification.Message == "Company not found");
     }
 
     [Fact]
-    public async Task ShouldReturnErrorResponseWhenRemoveCompanyPartnerThatDoesntExist()
+    public async Task ShouldReturnErrorResponseWhenRemovePartnerThatDoesntExist()
     {
         // arrange
 
+        var company = CompanyHelper.GenerateValidCompany();
+
+        _companyRepository.GetCompanyWithPartnersById(Arg.Any<Guid>())
+            .Returns(Task.FromResult<Company?>(company));
+
+        var command = new RemovePartner();
+
+        var handler = new RemovePartnerHandler(
+            companyRepository: _companyRepository,
+            unitOfWork: _unitOfWork
+        );
+
         // act
 
+        var response = await handler.Handle(command, new CancellationToken());
+
         // assert
+
+        Assert.Contains(response.Notifications, notification => notification.Message == "Partner not found");
     }
 
     [Fact]
-    public async Task ShouldRemoveCompanyPartnerWithSuccessWhenAExistingPartnerIsProvided()
+    public async Task ShouldRemovePartnerWithSuccessWhenAExistingPartnerIsProvided()
     {
         // arrange
 
+        var company = CompanyHelper.GenerateValidCompany();
+
+        _companyRepository.GetCompanyWithPartnersById(Arg.Any<Guid>())
+            .Returns(Task.FromResult<Company?>(company));
+
+        var command = new RemovePartner
+        {
+            PartnerId = new Guid("6c65317c-24bf-49b0-9d80-6ccf1c06658d"),
+            CompanyId = new Guid("b9ffc898-c3e4-4dfb-b1c6-86778f383f73")
+        };
+
+        var handler = new RemovePartnerHandler(
+            companyRepository: _companyRepository,
+            unitOfWork: _unitOfWork
+        );
+
         // act
 
+        var response = await handler.Handle(command, new CancellationToken());
+
         // assert
+
+        Assert.False(response.HasNotifications);
     }
 }
