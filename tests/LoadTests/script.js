6e18004bfcd4e32640b7fd7e36ba35051ace24a9
Tiago Santos <tfsantosbr@gmail.com>
2023-05-24 09:02:23 -0300

    add load tests

--- tests/LoadTests/script.js
+++ tests/LoadTests/script.js
@@ -6,9 +6,9 @@ import { randomString } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
 export const options = {
   stages: [
-    { duration: '60s', target: 10 },
-    { duration: '1m', target: 50 },
-    { duration: '10s', target: 100 },
+    { duration: '10s', target: 10 },
+    { duration: '20s', target: 50 },
+    { duration: '5s', target: 100 },
   ],
 };
 
